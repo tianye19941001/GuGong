fc788be71a5d610bb720f37dca9ad5e7235f8f09
tianye1 <838199892@qq.com>
2018-12-28 00:15:22 +0800

    page-3 ing

--- js/app.js
+++ js/app.js
@@ -56,17 +56,4 @@
 		}
 	}
 
-
-	$(window).scroll(function(){
-		
-		var scrollTop = $(this).scrollTop();    //滚动条距离顶部的高度
-		var scrollHeight = $(document).height();   //当前页面的总高度
-		var clientHeight = $(this).height();    //当前可视的页面高度
-		// console.log("top:"+scrollTop+",doc:"+scrollHeight+",client:"+clientHeight);
-		if(scrollTop + clientHeight >= scrollHeight){   //距离顶部+当前高度 >=文档总高度 即代表滑动到底部 
-		 	//滚动条到达底部
-		 	alert('到底了，这里触发js了')
-		}
-	});
-
 });
